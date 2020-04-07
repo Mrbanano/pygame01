0bdb02bee6dbab5740d92635f5e4565a73d6d4af
Alvaro <6666alvaro666@gmail.com>
2020-04-06 23:58:01 -0500

    first configuration
    +create def 
    +initial size screen
    +initial caption windows
    +initial funtions
    +prepare windows

---
+++ game/game.py
@@ -0,0 +1,39 @@
+import sys
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        self.surface = pygame.display.set_mode( (800,400) )
+        pygame.display.set_caption('SuperGame!!')
+
+        self.running = True 
+
+    def star(self):
+        
+        self.new()
+
+    def new(self):
+        
+        self.run()
+
+    def run (self):
+        
+        while self.running:
+            self.event()
+            self.draw()
+            self.update()
+
+    def event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running= False
+                pygame.QUIT
+                sys.exit()
+    def draw (self):
+        pass
+    def update (self):
+        pygame.display.flip()
+    def stop(self):
+        pass
